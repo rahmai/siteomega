1527f185e05e590c6b8bf724233396c5cf7ed6cb
OmegaCom <omegacom.site@gmail.com>
2017-12-27 19:58:46 -0200

    erro sass arrumado

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,16 @@
+
+'use strict';
+
 var gulp = require('gulp');
-var sass = require('gulp-ruby-sass');
+var sass = require('gulp-sass');
 
-// task para o sass
-gulp.task('sass',function(){
-
-	return sass('sass/*.sass')
-
+gulp.task('sass', function () {
+    return gulp.src('sass/**/*.sass')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('css'));
 });
 
-
-// task default gulp
-gulp.task('default', function(){
-
+gulp.task('sass:watch', function () {
+    gulp.watch('sass/**/*.sass', ['sass']);
 });
+
