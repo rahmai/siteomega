5662410f8d1bb733f2c6b15b69b3e682f4719bc5
OmegaCom <omegacom.site@gmail.com>
2017-12-28 18:25:27 -0200

    preparo dos asquivos css e sass

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,18 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var watch = require('gulp-watch');
 
 gulp.task('sass', function () {
     return gulp.src('sass/**/*.sass')
         .pipe(sass().on('error', sass.logError))
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(gulp.dest('css'));
 });
 
-gulp.task('sass:watch', function () {
+
+gulp.task('watch', function () {
     gulp.watch('sass/**/*.sass', ['sass']);
 });
 
+gulp.task('default',['sass','watch']);
